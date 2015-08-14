9425c979f6202f08648f4ae7edd12ad3861546a7
Dan Allen <dan.j.allen@gmail.com>
2015-08-13 23:48:06 -0600

    fix slide focus when using transform-based scaling

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,10 @@
 var deck = bespoke.from('#presentation', [
   bespoke.plugins.classes(),
   // scale plugin will attempt to use zoom in Chrome unless we specify transform
-  bespoke.plugins.scale('transform'),
+  bespoke.plugins.scale(),
+  //bespoke.plugins.scale('transform'),
+  // transform-group is a proposed change; see https://github.com/markdalgleish/bespoke-scale/pull/5
+  //bespoke.plugins.scale('transform-group'),
   bespoke.plugins.keys(),
   bespoke.plugins.fullscreen(),
   bespoke.plugins.forms()
