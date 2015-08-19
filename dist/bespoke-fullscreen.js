3c9856d9e27b847ab9db3f12c6150d909d8c4305
Dan Allen <dan.j.allen@gmail.com>
2015-08-18 22:23:30 -0600

    trap F11 to trigger fullscreen programmatically
    - trap F11 in addition to f
    - prevent default behavior when F11 is pressed
    - extract method to check if modifier key is down
    - remove jshint rule to enforce use of curly braces

--- dist/bespoke-fullscreen.js
+++ dist/bespoke-fullscreen.js
@@ -9,7 +9,7 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self);var f=n;f=f.bespoke||(f.bespoke={}),f=f.plugins||(f.plugins={}),f.fullscreen=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 module.exports = function(options) {
   return function(deck) {
-    var KEYCODE_F = 70,
+    var KEYCODE = { f: 70, f11: 122 },
       fullscreenEnabled = document.fullscreenEnabled || document.webkitFullscreenEnabled ||
           document.mozFullScreenEnabled || document.msFullscreenEnabled,
       toggleUserFullscreen = function() {
@@ -27,12 +27,19 @@ module.exports = function(options) {
         if (method) {
           method.apply(element);
         }
+      },
+      isModifierKeyDown = function(e) {
+        return e.ctrlKey || e.shiftKey || e.altKey || e.metaKey;
       };
 
     if (fullscreenEnabled) {
       document.addEventListener('keydown', function(e) {
-        if (e.which === KEYCODE_F) {
+        var keyCode = e.which;
+        if ((keyCode === KEYCODE.f || keyCode === KEYCODE.f11) && !isModifierKeyDown(e)) {
           toggleUserFullscreen();
+          if (keyCode === KEYCODE.f11) {
+            e.preventDefault();
+          }
         }
       }, false);
     }
