804297f055706c45e237ad2e4c3c957deea9f22b
Dan Allen <dan.j.allen@gmail.com>
2015-09-20 00:30:28 -0600

    add demo deployment; use bespoke-nav for navigation

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 var isWebKit = 'webkitAppearance' in document.documentElement.style;
 var deck = bespoke.from('.deck', [
   bespoke.plugins.classes(),
-  bespoke.plugins.keys(),
+  bespoke.plugins.nav(),
   bespoke.plugins.scale(isWebKit ? 'zoom' : 'transform'),
   bespoke.plugins.fullscreen(),
   bespoke.plugins.forms()
