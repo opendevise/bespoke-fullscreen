1b4b3c826a331222c54f26e61ac9083f46ae9090
Dan Allen <dan.j.allen@gmail.com>
2015-09-20 00:58:28 -0600

    write tests

--- test/spec/bespoke-fullscreenSpec.js
+++ test/spec/bespoke-fullscreenSpec.js
@@ -1,15 +1,20 @@
 Function.prototype.bind = Function.prototype.bind || require('function-bind');
 
 var bespoke = require('bespoke'),
-  fullscreen = require('../../lib-instrumented/bespoke-fullscreen.js');
+  fullscreen = require('../../lib/bespoke-fullscreen.js'),
+  simulant = require('simulant');
 
-describe("bespoke-fullscreen", function() {
-
-  var deck,
-    parent,
+describe('bespoke-fullscreen', function() {
+  var KEY = { f: 70, f11: 122 },
+    deck,
+    setup = function() {
+      document.fullscreenEnabled = true;
+      document.documentElement.requestFullscreen = jasmine.createSpy('requestFullscreen');
+      document.exitFullscreen = jasmine.createSpy('exitFullscreen');
+    },
     createDeck = function() {
-      parent = document.createElement('article');
-      for (var i = 0; i < 10; i++) {
+      var parent = document.createElement('article');
+      for (var i = 1; i <= 5; i++) {
         var section = document.createElement('section');
         parent.appendChild(section);
       }
@@ -21,24 +26,44 @@ describe("bespoke-fullscreen", function() {
       ]);
     },
     destroyDeck = function() {
-      if (parent != null) {
-        parent.parentNode.removeChild(parent);
-      }
+      deck.fire('destroy');
+      var parentNode = deck.parent.parentNode;
+      if (parentNode) parentNode.removeChild(deck.parent);
+      deck = null;
+    },
+    pressKey = function(which, opts, element) {
+      opts = opts ? opts : {};
+      opts.which = KEY[which] || which;
+      simulant.fire((element || document), 'keydown', opts);
     };
 
+  beforeAll(setup);
   beforeEach(createDeck);
   afterEach(destroyDeck);
 
-  describe("deck.slide", function() {
-
+  describe('toggle fullscreen', function() {
     beforeEach(function() {
-      deck.slide(0);
+      document.documentElement.requestFullscreen.calls.reset();
+      document.exitFullscreen.calls.reset();
+      document.fullscreenElement = undefined;
     });
 
-    it("will be a test one day", function() {
-      console.log("write me...");
-    });
+    ['f', 'f11'].forEach(function(key) {
+      it('requests fullscreen when ' + key + ' key is pressed', function() {
+        pressKey(key);
+        expect(document.documentElement.requestFullscreen.calls.count()).toBe(1);
+      });
 
+      it('exits fullscreen when ' + key + ' key is pressed', function() {
+        document.fullscreenElement = true;
+        pressKey(key);
+        expect(document.exitFullscreen.calls.count()).toBe(1);
+      });
+
+      it('does not request fullscreen when ' + key + ' key is pressed when modifier is pressed', function() {
+        pressKey(key, { shiftKey: true});
+        expect(document.documentElement.requestFullscreen.calls.count()).toBe(0);
+      });
+    });
   });
-
 });
